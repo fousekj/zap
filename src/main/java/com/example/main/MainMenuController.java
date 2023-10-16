137f018cbcb5417e6f345d8a636d5f330c2cd6cc
Jiri Fousek <fou0027@vsb.cz>
2023-10-16 10:08:47 +0200

    zprovozneni main menu

---
+++ src/main/java/com/example/main/MainMenuController.java
@@ -0,0 +1,50 @@
+package com.example.main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainMenuController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void createSalesOrg() {
+
+    }
+
+    @FXML
+    protected void changeSalesOrg() {
+        //welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void displaySalesOrg() {
+    }
+
+    public void createDocType(ActionEvent actionEvent) {
+    }
+
+    public void changeDocType(ActionEvent actionEvent) {
+    }
+
+    public void displayDocType(ActionEvent actionEvent) {
+    }
+
+    public void createCustomer(ActionEvent actionEvent) {
+    }
+
+    public void changeCustomer(ActionEvent actionEvent) {
+    }
+
+    public void displayCustomer(ActionEvent actionEvent) {
+    }
+
+    public void createMaterial(ActionEvent actionEvent) {
+    }
+
+    public void changeMaterial(ActionEvent actionEvent) {
+    }
+
+    public void displayMaterial(ActionEvent actionEvent) {
+    }
+}
